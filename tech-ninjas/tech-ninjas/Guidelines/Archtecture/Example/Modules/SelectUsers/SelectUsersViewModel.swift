cbe3b4114e0a04e42e2170b4fc146a1537255fb2
Felipe Melo <feasm159@gmail.com>
2022-10-05 00:47:42 -0300

    New MVVM

--- tech-ninjas/tech-ninjas/Guidelines/Archtecture/Example/Modules/SelectUsers/SelectUsersViewModel.swift
+++ tech-ninjas/tech-ninjas/Guidelines/Archtecture/Example/Modules/SelectUsers/SelectUsersViewModel.swift
@@ -12,71 +12,48 @@ import RxCocoa
 import TNCore
 import TNUI
 
-struct SelectUsersViewModelInput {
-    var searchText: Observable<String?>
-}
-
-struct SelectUsersViewModelOutput {
-    var users: Driver<[UserModel]>
-}
-
 protocol SelectUsersViewModel {
-    var output: SelectUsersViewModelOutput { get }
+    var filteredUserModels: [UserModel] { get }
     
-    func setup(_ input: SelectUsersViewModelInput)
+    var didUpdateUsers: (() -> Void)? { get set }
+    
     func getUsers()
+    func filterUsers(text: String)
 }
 
 final class SelectUsersViewModelImpl: SelectUsersViewModel {
     
-    var input: SelectUsersViewModelInput?
-    var output: SelectUsersViewModelOutput
-    
     private let service: SelectUsersService
-    private let disposeBag = DisposeBag()
+    private var userModels = [UserModel]()
     
-    private var users = [UserModel]() {
-        didSet {
-            updateUsers.onNext(users)
-        }
-    }
-    private var updateUsers = PublishSubject<[UserModel]>()
+    var filteredUserModels = [UserModel]()
+    
+    var didUpdateUsers: (() -> Void)?
     
     init(service: SelectUsersService) {
         self.service = service
-        
-        output = SelectUsersViewModelOutput(
-            users: updateUsers.asDriver(onErrorJustReturn: [])
-        )
-    }
-    
-    func setup(_ input: SelectUsersViewModelInput) {
-        self.input = input
-        
-        input
-            .searchText
-            .compactMap({ $0 })
-            .subscribe(onNext: { [weak self] searchText in
-                guard let self = self else { return }
-                
-                let filteredUsers =
-                    searchText.isEmpty ?
-                    self.users : self.users.filter({ $0.name.contains(searchText) })
-                
-                self.updateUsers.onNext(filteredUsers)
-            })
-            .disposed(by: disposeBag)
     }
     
     func getUsers() {
-        service
-            .getUsers()
-            .subscribe { [weak self] userModels in
-                self?.users = userModels
-            } onFailure: { error in
-                print(error)
+        service.getUsers { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let userModels):
+                self.userModels = userModels
+                self.filteredUserModels = userModels
+                
+                self.didUpdateUsers?()
+            case .failure(let error):
+                print(error.localizedDescription)
             }
-            .disposed(by: disposeBag)
+        }
+    }
+    
+    func filterUsers(text: String) {
+        filteredUserModels = text.isEmpty ? self.userModels : self.userModels.filter({ $0.name.contains(text) })
+        
+        didUpdateUsers?()
     }
     
 }
@@ -86,30 +63,33 @@ struct UserModel: Decodable {
     let description: String
 }
 
+enum NetworkError: Error {
+    case internalServerError
+}
+
 protocol SelectUsersService {
-    func getUsers() -> Single<[UserModel]>
+    func getUsers(completion: @escaping (Result<[UserModel], NetworkError>) -> Void)
 }
 
 final class SelectUsersServiceImpl: SelectUsersService {
     
-    func getUsers() -> Single<[UserModel]> {
-        // buscar do mongoDB
-        return .just([])
+    func getUsers(completion: @escaping (Result<[UserModel], NetworkError>) -> Void) {
+        
     }
     
 }
 
 final class SelectUsersServiceMock: SelectUsersService {
     
-    func getUsers() -> Single<[UserModel]> {
-        return .just([
+    func getUsers(completion: @escaping (Result<[UserModel], NetworkError>) -> Void) {
+        completion(.success([
             .init(name: "Carlinhos", description: "Pai do carlinhozinho"),
             .init(name: "Minatti", description: "Pai do toddy"),
             .init(name: "Po", description: "Pai do futuro"),
             .init(name: "Carlinhozinho", description: "Filho do carlinhos"),
             .init(name: "Toddy", description: "Filho do minatti"),
             .init(name: "Futuro", description: "Filho do paulo")
-        ])
+        ]))
     }
     
 }
